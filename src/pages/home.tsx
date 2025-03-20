d5b6d571f536d7f745330b7796c1fa4b1dc0bde9
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2025-03-20 15:38:32 -0300

    feat: created home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,28 @@
+import { Social } from "../componets/social";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
 export const Home: React.FC = () => {
   return (
-    <div className="h-screen w-full bg-neutral-900">
-      <div className="text-3xl text-white">Página home</div>
+    <div className="flex w-full flex-col items-center justify-center py-4">
+      <h1 className="mt-20 text-3xl font-bold text-white md:text-4xl">
+        Gabriel Frontend
+      </h1>
+      <span className="mt-3 mb-5 text-white">Veja meus links 👇</span>
+      <main className="flex w-11/12 max-w-xl flex-col text-center">
+        <section className="mb-4 w-full cursor-pointer rounded-lg bg-white py-2 transition-transform select-none hover:scale-105">
+          <a>
+            <p className="text-base md:text-lg">Linkedin</p>
+          </a>
+        </section>
+        <footer className="my-4 flex justify-center gap-3">
+          <Social url="https://www.linkedin.com/in/gabriel-moura-b63382161/">
+            <FaLinkedin size={35} color="#fff" />
+          </Social>
+          <Social url="https://github.com/gbmouraa">
+            <FaGithub size={35} color="#fff" />
+          </Social>
+        </footer>
+      </main>
     </div>
   );
 };
